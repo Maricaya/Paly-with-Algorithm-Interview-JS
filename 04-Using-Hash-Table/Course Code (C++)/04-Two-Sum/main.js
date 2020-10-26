4b1f03c59e987534868e9b8b1b9385f36e32eae4
Maricaya <43136257+Maricaya@users.noreply.github.com>
2020-10-26 20:51:59 +0800

    Update main.js

--- 04-Using-Hash-Table/Course Code (C++)/04-Two-Sum/main.js
+++ 04-Using-Hash-Table/Course Code (C++)/04-Two-Sum/main.js
@@ -16,6 +16,85 @@ function twoSum(nums, target) {
 
 console.log(twoSum([2, 7, 11, 15], 9))
 
+function twoSum1(nums, target) {
+    // nums.sort((a, b) => a - b)
+    // let l = 0, r = nums.length-1
+    // while (l< r) {
+    //     if (nums[l] + nums[r] < target) {
+    //         l++
+    //     } else if (nums[l] + nums[r] > target) {
+    //         r--
+    //     } else {
+    //         return [l, r]
+    //     }
+    // }
+}
+console.log(twoSum1([3, 3], 6))
 // 练习题 15. 3sum
+function sum3(nums, target) {
+    // nums.sort((a, b) => a - b)
+    // const result = []
+    // for (let i = 0; i < nums.length - 2; i++) {
+    //     if(i > 0 && nums[i] === nums[i-1]) continue; // 去重
+    //     let l = i+1, r = nums.length-1
+    //     while (l< r) {
+    //         let sum = nums[l] + nums[r] + nums[i]
+    //         if (sum < target) {
+    //             l++
+    //         } else if (sum > target) {
+    //             r--
+    //         } else {
+    //             result.push([nums[i],nums[l],nums[r]])
+    //             // 去重
+    //             while (l<r && nums[l] === nums[l+1]) { l++ }
+    //             while (l<r && nums[r] === nums[r-1]) { r-- }
+    //             l++;r--;
+    //         }
+    //     }
+    // }
+    // return result
+}
+// console.log(sum3([-1,0,1,2,-1,-4], 0))
 // 18. 4sum
+const fourSum = function (nums, target) {
+    const result = []
+    for (let i = 0; i < nums.length-3; i++) {
+        if (i > 0 && nums[i] === nums[i-1]) continue
+        const threeResult = sum3(nums.slice(i+1), target - nums[i])
+        threeResult.map(item => {
+            result.push([nums[i], ...item])
+        })
+    }
+    return result
+}
+fourSum([1,0,-1,0,-2,2],  0)
 // 16. 3sum closest
+function threeSumClosest(nums, target) {
+    nums.sort((a, b) => a - b)
+    const result = []
+    let abs = Infinity
+    for (let i = 0; i < nums.length - 2; i++) {
+        // if(i > 0 && nums[i] === nums[i-1]) continue; // 去重
+        let l = i+1, r = nums.length-1
+        while (l< r) {
+            let sum = nums[l] + nums[r] + nums[i]
+            if (Math.abs(sum - target) <  abs ) {
+                abs = sum
+            }
+            if (sum < target) {
+                l++
+            } else if (sum > target) {
+                r--
+            } else {
+                result.push([nums[i],nums[l],nums[r]])
+                // 去重
+                // while (l<r && nums[l] === nums[l+1]) { l++ }
+                // while (l<r && nums[r] === nums[r-1]) { r-- }
+                l++;r--;
+                return abs
+            }
+        }
+    }
+    return result
+}
+
